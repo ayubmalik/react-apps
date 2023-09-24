15b30e01e791501903b67e789669986f223fe72a
Ayub Malik <ayub.malik@gmail.com>
2023-09-24 19:08:57 +0100

    tidy up fake-server

--- buzzlines/fake-server.go
+++ buzzlines/fake-server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 )
@@ -11,6 +12,7 @@ type City struct {
 	Name string `json:"name"`
 }
 
+// Don't use a library so can run easily from `npm`
 func main() {
 	origins := []City{
 		{1, "Manchester"},
@@ -18,14 +20,13 @@ func main() {
 		{3, "Hull"},
 		{4, "Liverpool"},
 	}
+
 	destinations := map[string][]City{
 		"1": {{2, "Bradford"}, {3, "Hull"}},
 		"2": {{3, "Hull"}},
-		"3": {},
 		"4": {{1, "Manchester"}, {3, "Hull"}},
 	}
 
-	// Don't use a library so can run file directly
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		log.Println(r.URL.Path)
 		w.Header().Set("Content-Type", "application/json")
@@ -36,17 +37,19 @@ func main() {
 		}
 
 		if r.URL.Path == "/destinations" {
-			q := r.URL.Query()
-			origin := q.Get("origin")
-			if origin != "" {
-				json.NewEncoder(w).Encode(destinations[origin])
-				return
+			origin := r.URL.Query().Get("origin")
+			if dest, ok := destinations[origin]; ok {
+				json.NewEncoder(w).Encode(dest)
+			} else {
+				json.NewEncoder(w).Encode([]City{})
 			}
+			return
 		}
 		w.WriteHeader(http.StatusNotFound)
 	})
 
-	log.Println("http://localhost:30001/origins")
-	log.Println("http://localhost:30001/destinations?origin=1")
-	log.Fatalln(http.ListenAndServe(":3001", nil))
+	port := 3001
+	log.Printf("http://localhost:%d/origins\n", port)
+	log.Printf("http://localhost:%d/destinations?origin=1\n", port)
+	log.Fatalln(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
 }
