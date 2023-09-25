50156f9745e2d4c22b2a23ba72cd4895a4e4a604
Ayub Malik <ayub.malik@coachusa.com>
2023-09-25 13:39:09 +0100

    Added test-server using json-server

---
+++ buzzlines/test-server.js
@@ -0,0 +1,45 @@
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router({}) // no db.json
+const middlewares = jsonServer.defaults()
+server.use(middlewares)
+
+const port = 3001
+// in memory db
+const origins = [
+    { "id": 1, "name": "Manchester" },
+    { "id": 2, "name": "Bradford" },
+    { "id": 3, "name": "Leeds" },
+    { "id": 4, "name": "Hull" }
+]
+
+const destinations = {
+    "1": [
+        { "id": 2, "name": "Bradford" }
+    ]
+}
+
+// custom routes
+server.get('/origins', (req, res) => {
+    res.json(origins)
+})
+
+server.get('/destinations', (req, res) => {
+    console.log("query", req.query)
+    const origin = req.query.origin
+    result = destinations[origin]
+    if (result) {
+        console.log("destinations result", result)
+        res.json(result)
+    } else {
+        res.sendStatus(404)
+    }
+})
+
+// use default router and middlewares
+server.use(router)
+server.listen(port, () => {
+    console.log('JSON Server is running')
+    console.log(`http://localhost:${port}/origins`)
+    console.log(`http://localhost:${port}/destinations?origin=1`)
+})
